c4b28bc0c8e43763c5197f0f90316d100086362d
GuiBrandt <gui.g.brandt@gmail.com>
2019-12-21 23:55:04 -0300

    Fix next_token on symbol followed by something

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -3,27 +3,39 @@
 #include "token.h"
 
 size_t next_token(const char* string, size_t length, token* tk) {
+    if (tk)
+        tk->type = TOKEN_NAME;
+
     size_t i, skip = 0;
+
     for (i = 0; i < length; i++) {
         char c = string[i];
+
         if (c == '"') {
             // TODO: String
         } else if (c == '#') {
             // TODO: Comment
         } else if (isspace(c)) {
-            while (i + skip < length && isspace(string[i + skip])) skip++;
+            while (i + skip < length && isspace(string[i + skip]))
+                skip++;
             break;
-        } else if (i > 0 && ispunct(c))
-            break;
+        } else if (ispunct(c)) {
+            if (i > 0)
+                break;
+
+            if (tk) {
+                tk->type = TOKEN_SYMBOL;
+                tk->value.small.data[i++] = c;
+                break;
+            }
+        }
 
         if (tk)
             tk->value.small.data[i] = c;
     }
 
-    if (tk) {
+    if (tk)
         tk->value.small.length = i;
-        tk->type = TOKEN_NAME;
-    }
     
     return i + skip;
 }
