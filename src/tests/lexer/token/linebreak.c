74188163c1a2565fc830175ccece78ac26b7a7f3
GuiBrandt <gui.g.brandt@gmail.com>
2020-01-06 15:56:24 -0300

    Implement comment tokens & linebreaks

--- src/tests/lexer/token/linebreak.c
+++ src/tests/lexer/token/linebreak.c
@@ -1,23 +1,29 @@
 #include <assert.h>
 #include <string.h>
 
+#include <stdio.h>
+
 #include <lexer/token.h>
 
 int main() {
-    int length = 28;
+    int length = 7;
     char const* string = "abc\nghi";
 
-    next_token(string, length, NULL);
+    size_t i = next_token(string, length, NULL);
+    string += i;
+    length -= i;
 
     token t;
     
-    size_t i = next_token(string, length, &t);
+    i = next_token(string, length, &t);
     string += i;
     length -= i;
 
     assert(t.type == TOKEN_LINEBREAK);
 
     next_token(string, length, &t);
+
+    fprintf(stderr, "%d %d %s", i, t.value.small.length, t.value.small.data);
 
     assert(t.value.small.length == 3);
     assert(memcmp(t.value.small.data, "ghi", 3) == 0);
