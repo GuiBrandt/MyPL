49d2f58eaae391e9d16a61472d10267e64072e23
GuiBrandt <gui.g.brandt@gmail.com>
2020-01-06 02:42:58 -0300

    Implement string tokens

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -1,5 +1,7 @@
 #include <ctype.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <util/trie_set.h>
@@ -10,17 +12,53 @@ bool issymbol(char c) { return c != '_' && ispunct(c); }
 bool isoperator(const char* c);
 bool potential_operator(const char* c);
 
+typedef struct _large_token_block large_token_block;
+
 size_t next_token(register const char* string, register size_t length, token* tk) {
-    if (tk)
+    if (tk) {
+        tk->value.small.length = 0;
         tk->type = TOKEN_NAME;
+    }
 
     register size_t i, skip = 0;
 
     for (i = 0; i + skip < length; i++) {
         register char c = string[i + skip];
 
-        if (c == '"') {
-            // TODO: String
+        if (c == '"' || c == '\'') {
+            large_token_block* block = &tk->value.large;
+
+            bool escape = false;
+
+            for (skip++; escape || string[i + skip] != c; i++) {
+                if (i + skip >= length)
+                    exit(-1);
+
+                if (string[i + skip] == '\\')
+                    escape ^= true;
+                else
+                    escape = false;
+
+                if (!tk)
+                    continue;
+                
+                if (block->length < sizeof(block->data)) {
+                    block->data[i] = string[i + skip];
+                    block->length++;
+                } else {
+                    large_token_block* next = (large_token_block*)malloc(sizeof(large_token_block));
+                    next->length = 0;
+                    next->next = NULL;
+                    block->next = next;
+                    block = next;
+                }
+            }
+            
+            skip++;
+
+            if (tk)
+                tk->type = TOKEN_STRING;
+            break;
         } else if (c == '#') {
             // TODO: Comment
         } else if (isspace(c)) {
@@ -40,9 +78,10 @@ size_t next_token(register const char* string, register size_t length, token* tk
             char val[4] = {0};
             register size_t j;
 
-            for (j = 0;
-                 j < 4 && i < length && issymbol(string[i + skip]);
-                 i++, j++) {
+            for (j = 0; j < 4 && i < length; i++, j++) {
+                if (!issymbol(string[i + skip]))
+                    break;
+                
                 val[j] = string[i + skip];
                 if (j > 0 && !potential_operator(val)) {
                     val[j] = 0;
@@ -56,14 +95,9 @@ size_t next_token(register const char* string, register size_t length, token* tk
                 tk->value.small.length = j;
             }
             break;
-        }
-
-        if (tk)
-            tk->value.small.data[i] = c;
+        } else if (tk)
+            tk->value.small.data[tk->value.small.length++] = c;
     }
-
-    if (tk)
-        tk->value.small.length = i;
     
     return i + skip;
 }
--- src/tests/lexer/token/gets_string.c
+++ src/tests/lexer/token/gets_string.c
@@ -1,18 +1,26 @@
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <lexer/token.h>
 
 int main() {
-    int length = 17;
+    int length = 16;
     char const* string = "\"abc defg\" hijkl";
     token t;
     
+    size_t i = next_token(string, length, &t);
+
+    assert(t.value.large.length == 8);
+    assert(memcmp(t.value.large.data, "abc defg", 8) == 0);
+    assert(t.type == TOKEN_STRING);
+
+    string += i;
+    length -= i;
     next_token(string, length, &t);
 
-    assert(t.value.small.length == 8);
-    assert(memcmp(t.value.small.data, "abc defg", 8) == 0);
-    assert(t.type == TOKEN_STRING);
+    assert(t.value.small.length == 5);
+    assert(memcmp(t.value.small.data, "hijkl", 5) == 0);
 
     return 0;
 }
