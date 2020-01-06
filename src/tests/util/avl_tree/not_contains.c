e803dbf2fa0a1c51866c78bf0d690c36037a761f
GuiBrandt <gui.g.brandt@gmail.com>
2020-01-06 18:26:27 -0300

    Cover avl_tree right rotation

--- src/tests/util/avl_tree/not_contains.c
+++ src/tests/util/avl_tree/not_contains.c
@@ -7,11 +7,15 @@ int main() {
     
     avl_add(&t, 1, "a");
     avl_add(&t, 2, "b");
-    avl_add(&t, 3, "c");
     avl_add(&t, 4, "d");
+    avl_add(&t, 3, "c");
+    avl_add(&t, 9, "a");
+    avl_add(&t, 8, "b");
+    avl_add(&t, 6, "d");
+    avl_add(&t, 7, "c");
 
-    assert(avl_get(&t, 6) == 0);
     assert(avl_get(&t, 10) == 0);
+    assert(avl_get(&t, 20) == 0);
     assert(avl_get(&t, 0) == 0);
     assert(avl_get(&t, 5) == 0);
     
